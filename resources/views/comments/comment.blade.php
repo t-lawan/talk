ca343d078c459f762ac106cb3079fd97f80b3142
jellyfish <thomas.lawanson@gmail.com>
2017-05-01 09:12:33 +0100

    Changed Comments

--- resources/views/comments/comment.blade.php
+++ resources/views/comments/comment.blade.php
@@ -1,13 +1,17 @@
-  <li class="media">
-    <img class="d-flex mr-3" src="http://placehold.it/70" alt="Generic placeholder image">
-    <div class="media-body">
-      <h5 class="mt-0 mb-1">
-        <a href="/posts/{{$post->id}}">
-          Username <small class="text-muted"> <i>Posted  {{$comment->created_at->diffForHumans()}}</i></small>
-        </a>
-      </h5>
-      <p>
-        {{$comment->body}}
-      </p>
+  <div class="panel panel-default col-md-10 col-md-offset-1">
+    <div class="panel-body">
+      <li class="media">
+        <img class="d-flex mr-3" src="http://placehold.it/70" alt="Generic placeholder image">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1">
+            <a href="/posts/{{$post->id}}">
+              {{$comment->user->name}} <small class="text-muted"> <i>Posted  {{$comment->created_at->diffForHumans()}}</i></small>
+            </a>
+          </h5>
+          <p>
+            {{$comment->body}}
+          </p>
+        </div>
+      </li>
     </div>
-  </li>
+  </div>
--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -1,43 +1,20 @@
 <!-- Create Comment-->
-<div class="container-fluid create-posts">
 
-        <div class="panel panel-default ">
 
+
+
+        <div class="panel panel-default col-md-10 col-md-offset-1" id="comment_form">
+          <div class="panel-body">
             <form method="post" action="/posts/{{$post->id}}/comments">
               <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div class="panel-heading">
+
                     <div class="form-group">
-                        <textarea class="form-control input-lg no-border" rows="2" id="comment_body" name="body" placeholder="Your comment here." required="true"></textarea>
+                        <textarea class="form-control input-lg no-border" rows="2" name="body" placeholder="Your comment here." required="true"></textarea>
                     </div>
-                </div>
-                @include('layouts.errors')
+                    @include('layouts.errors')
 
-                  <button type="submit" class="btn btn-outline-secondary offset-sm-9 btn-lg" id ='commentsubmitbutton'>Add Comment</button>
-
-
+                      <button type="submit" class="btn btn-outline-secondary btn-block offset-sm-9">Add Comment</button>
 
             </form>
-
+          </div>
         </div>
-
-
-
-
-</div>
-
-
-<script>
-
-  $(document).ready(function()
-  {
-    $('#commentsubmitbutton').hide();
-  });
-
-  $('#comment_body').on('focus',function(){
-    $('#commentsubmitbutton').show();
-  })
-
-
-
-
-</script>
