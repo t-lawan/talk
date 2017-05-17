4bf7d579ea137211097429698746da42f71d5af8
jellyfish <thomas.lawanson@gmail.com>
2017-05-17 11:35:07 +0100

    responsive to mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,24 @@
-<html>
+<html lang="{{ config('app.locale') }}">
     <head>
-        <title>Talk</title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        @include('layouts.links')
-        
+      <!-- CSRF Token -->
+      <meta name="csrf-token" content="{{ csrf_token() }}">
 
+      <title>Talk</title>
+
+      <!-- Styles -->
+      @include('layouts.links')
+      <!-- Scripts -->
+      <script>
+          window.Laravel = {!! json_encode([
+              'csrfToken' => csrf_token(),
+          ]) !!};
+      </script>
     </head>
+
 
     <body>
 
